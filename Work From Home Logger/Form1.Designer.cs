3a4809da54fa6c8f737adad72202101967570616
Scott Schubert <scott_schubert@hotmail.com.au>
2021-09-14 21:05:43 +0930

    Starting on building out the form

--- Work From Home Logger/Form1.Designer.cs
+++ Work From Home Logger/Form1.Designer.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Windows.Forms;
+
 namespace Work_From_Home_Logger
 {
     partial class Form1
@@ -7,6 +9,7 @@ namespace Work_From_Home_Logger
         ///  Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
+
 
         /// <summary>
         ///  Clean up any resources being used.
@@ -29,72 +32,63 @@ namespace Work_From_Home_Logger
         /// </summary>
         private void InitializeComponent()
         {
-            this.addButton = new System.Windows.Forms.Button();
-            this.currentValue = new System.Windows.Forms.TextBox();
-            this.subtractButton = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // addButton
+            // panel1
             // 
-            this.addButton.Location = new System.Drawing.Point(290, 139);
-            this.addButton.Name = "addButton";
-            this.addButton.Size = new System.Drawing.Size(188, 58);
-            this.addButton.TabIndex = 0;
-            this.addButton.Text = "Increment";
-            this.addButton.UseVisualStyleBackColor = true;
-            this.addButton.Click += new System.EventHandler(this.Add);
-            // 
-            // currentValue
-            // 
-            this.currentValue.Location = new System.Drawing.Point(290, 268);
-            this.currentValue.Name = "currentValue";
-            this.currentValue.Size = new System.Drawing.Size(250, 47);
-            this.currentValue.TabIndex = 1;
-            // 
-            // subtractButton
-            // 
-            this.subtractButton.Location = new System.Drawing.Point(607, 139);
-            this.subtractButton.Name = "subtractButton";
-            this.subtractButton.Size = new System.Drawing.Size(188, 58);
-            this.subtractButton.TabIndex = 2;
-            this.subtractButton.Text = "decrement";
-            this.subtractButton.UseVisualStyleBackColor = true;
-            this.subtractButton.Click += new System.EventHandler(this.Subtract);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(121, 84);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(50);
+            this.panel1.Size = new System.Drawing.Size(496, 938);
+            this.panel1.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(88, 60);
+            this.label1.Location = new System.Drawing.Point(53, 75);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(97, 41);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "label1";
+            this.label1.Size = new System.Drawing.Size(225, 41);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Your IP Address";
             this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(53, 139);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(390, 47);
+            this.textBox1.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 41F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.subtractButton);
-            this.Controls.Add(this.currentValue);
-            this.Controls.Add(this.addButton);
+            this.ClientSize = new System.Drawing.Size(2003, 1142);
+            this.Controls.Add(this.panel1);
             this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(50);
             this.Text = "Work from home logger";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
+
+
+
         #endregion
 
-        private System.Windows.Forms.Button addButton;
-        private System.Windows.Forms.TextBox currentValue;
-        private System.Windows.Forms.Button subtractButton;
-        private System.Windows.Forms.Label label1;
+        private Panel panel1;
+        private Label label1;
+        private TextBox textBox1;
     }
 }
 
--- Work From Home Logger/Form1.cs
+++ Work From Home Logger/Form1.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,36 +17,36 @@ namespace Work_From_Home_Logger
         public Form1()
         {
             InitializeComponent();
+            // Add this to the form class's constructor.  
+            var addresses = GetIpAddress();
+            for (int i = 0; i < addresses.Length; i++)
+            {
+                Debug.WriteLine($"{i}, {addresses[i]}");
+            }
         }
 
-        public int Counter { get; set; }
+        public string IpAddress { get; set; }
+        public string HostName { get; private set; }
+
+        public string GetHostName()
+        {
+            var hostName = Dns.GetHostName();
+            Debug.WriteLine(hostName);
+            return hostName;
+        }
+
+        public IPAddress[] GetIpAddress()
+        {
+            var hostName = GetHostName();
+            IPAddress[] addresses = Dns.GetHostAddresses(hostName);
+
+            return addresses;
+        }
+
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            currentValue.Text = Counter.ToString();
-        }
-
-        private void SetCurrentValue()
-        {
-            currentValue.Text = Counter.ToString();
-        }
-
-
-        private void Add(object sender, EventArgs e)
-        {
-            Counter += 1;
-            SetCurrentValue();
-        }
-
-        private void Subtract(object sender, EventArgs e)
-        {
-            if(Counter <= 0)
-            {
-                label1.Text = "Cannot go below 0";
-                return;
-            }
-            Counter -= 1;
-            SetCurrentValue();
+            
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -53,3 +55,7 @@ namespace Work_From_Home_Logger
         }
     }
 }
+
+// Save an IP address
+// Save a workday
+
